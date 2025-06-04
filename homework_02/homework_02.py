6eb0d7f21549e26595fd5e0cffcbbf97c960e6b4
katemamyan <35809652+katemamyan@users.noreply.github.com>
2025-06-04 19:57:33 +0400

    Update homework_02.py

--- homework_02/homework_02.py
+++ homework_02/homework_02.py
@@ -1,3 +1,3 @@
-# Katya Mamyan
-# 01.10.1995
-# Homework 2
+age = int(input("How old are you?"))
+print(f'Your heart has beaten {age*35000000} times in that time') 
+print(f'the blue whales heart rate can drop to as low as 2 beats per minute and can increase to about 27 beats per minute. So the heart rate can be between {age*2*60*24*365} and {age*27*60*24*365}')
